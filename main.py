607533bd14153728c52f29055feab572ce885d5e
chaitanya44444 <93338022+chaitanya44444@users.noreply.github.com>
2023-05-14 23:09:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 
 
-# Prompt the user to input the webhook URL and message to send
+# Prompt for webhook url 
 webhook_url = input("Please enter the Discord webhook URL: ")
 
 
@@ -14,38 +14,42 @@ webhook_url = input("Please enter the Discord webhook URL: ")
 
 
 method1=int(input("Do you want to send msgs like a CHAT Or 1 msg with a number of tiems to send it write 1 for chat and 2 for second metoh"))
-if method==1:
-    # Define an infinite loop to continuously prompt the user for input and send messages
+if method1==1:
+ 
   while True:
-      # Prompt the user to input a message to send
+      # Prompt  for msg
       message = input("Please enter the message to send: ")
 
-      # Send the message to the channel using the webhook
+     
       data = {"content": message}
       response = requests.post(webhook_url, json=data)
 
-      # Display a success or failure message depending on the response status code
+      
       if response.status_code == 204:
           print("Message sent successfully!")
       else:
-          print(f"Failed to send message. Status code: {response.status_code}")'''
+          print(f"Failed to send message. Status code: {response.status_code}")
 
 if method1==2:  
-  # Prompt the user to input the number of times to send the message
+ 
   num_times = int(input("Please enter the number of times to send the message: "))
   message = input("Please enter the message to send: ")
-  # Loop through the messages and send them using the webhook
-  for i in range (2):
+ 
 
-    for i in range(num_times):
-        data = {"content": message}
-        time.sleep(3)
-        response = requests.post(webhook_url, json=data)
 
-        if response.status_code == 204:
-            print(f"Message {i+1} sent successfully!")
-        else:
-            print(f"Failed to send message {i+1}. Status code: {response.status_code}")
+  for i in range((num_times)+1):
+
+      time.sleep(3)
+
+      
+      data = {"content": message}
+      response = requests.post(webhook_url, json=data)
+
+    
+      if response.status_code == 204:
+          print("Message sent successfully!")
+      else:
+          print(f"Failed to send message. Status code: {response.status_code}")
       
          
 
